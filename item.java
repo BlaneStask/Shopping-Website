e00b9d41f05bfa826fb86c58514d6fc0b245b240
Blane Staskiewicz <bas6248@psu.edu>
2020-12-30 14:53:25 -0500

    Update item.java

--- item.java
+++ item.java
@@ -1,13 +1,11 @@
 package shopc;
 
 public class item{
-	
 	int number;
 	String name;
 	String category;
 	double price;
 	int quantity;
-	
 	public item(int nu, String na, String ca, double pr, int qu) {
 		number = nu;
 		name = na;
@@ -19,8 +17,8 @@ public class item{
 	 * 
 	 * @return		values to for item in cart
 	 */
-	public String toString() {
-		return ("Number:   " + number + "\n" +
+	public String toString(){
+		return("Number:   " + number + "\n" +
 		"Name:   " + name + "\n" +
 		"Category:   " + category + "\n" +
 		"Price:   " + price + "\n" +
@@ -28,67 +26,67 @@ public class item{
 		);
 	}
 	
-	public void stringtoFlat() {
+	public void stringtoFlat(){
 		System.out.println(number + "	" + name + "			 $" + price);
 	}
 	/** This method returns number
 	 * 
 	 * @return	number
 	 */
-	public int getnumber() {
+	public int getnumber(){
 		return number;
 	}
 	/** This method returns name
 	 * 
 	 * @return	name
 	 */
-	public String getname() {
+	public String getname(){
 		return name;
 	}
 	/** This method returns category
 	 * 
 	 * @return	category
 	 */
-	public String getcategory() {
+	public String getcategory(){
 		return category;
 	}
 	/** This method returns price
 	 * 
 	 * @return	price
 	 */
-	public double getprice() {
+	public double getprice(){
 		return price;
 	}
 	/** This method returns quantity
 	 * 
 	 * @return	quantity	
 	 */
-	public int getquantity() {
+	public int getquantity(){
 		return quantity;
 	}
 	/** This method sets number
 	 */
-	public void setnumber(int number) {
+	public void setnumber(int number){
 		this.number = number;
 	}
 	/** This method sets name
 	 */
-	public void setname(String name) {
+	public void setname(String name){
 		this.name = name;
 	}
 	/** This method sets category
 	 */
-	public void setcategory(String category) {
+	public void setcategory(String category){
 		this.category = category;
 	}
 	/** This method sets price
 	 */
-	public void setprice(double price) {
+	public void setprice(double price){
 		this.price = price;
 	}
 	/** This method sets quantity
 	 */
-	public void setquantity(int quantity) {
+	public void setquantity(int quantity){
 		this.quantity = quantity;
 	}
 }
